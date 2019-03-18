8705fcb70d7689127e63a6a05cc0e6faced52d41
Berkmann18 <maxieberkmann@gmail.com>
2019-03-18 17:11:39 +0000

    New: Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#0cf' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/apollo'
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  },
+
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: 'https://graphql.datocms.com',
+        getAuth: () => 'Bearer f65dc0c42001b12657e2d8ccd3ef98'
+      },
+    }
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        nuxty
+      </h1>
+      <h2 class="subtitle">
+        My CMSed Static Nuxt Project
+      </h2>
+      <div>
+        <ul>
+          <li
+            v-for="(post, index) in allPosts"
+            :key="index"
+          >
+            <h2>
+              <nuxt-link :to="`/blog/${post.slug}`">
+                {{ post.title }}
+              </nuxt-link>
+            </h2>
+            <p>{{ post.text }}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    allPosts: gql`{
+      allPosts {
+        title
+        text
+        slug
+      }
+    }`
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
